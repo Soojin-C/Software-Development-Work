98e106410037a2b50138abd05f35a9ca95ca61f4
Soojin Choi <schoi4@stuy.edu>
2018-10-08 23:19:05 -0400

    k17

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -9,54 +9,61 @@ DB_FILE = "discobandit.db"
 db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
+#dictionary that holds all the students' data
 studentGrades = {}
 
-name_id = c.execute("SELECT name, id FROM peeps;")
+#looks up the students grade and keeps them.
+#studentsGrades should be studentName : [id , [grades...]]
+def look_up():
+    name_id = c.execute("SELECT name, id FROM peeps;")
 
-#Creates a dictionary with the key as the name of the student and holds the value of an array with the students id.
-for each_peeps in name_id:
+    #loops through to get the keys and id for each student
+    for each_peeps in name_id:
 
-    peepsName = each_peeps[0]
-    peepsId = each_peeps[1]
-    studentGrades[peepsName] = peepsId
+        peepsName = each_peeps[0]
+        peepsId = each_peeps[1]
+        studentGrades[peepsName] = peepsId
 
-#Loops through the dictionary and pops of the id to retrieve it.
-for each in studentGrades:
-    #print (each)
-    targetID = studentGrades[each]
-    #print (targetID)
+    #loops through the dictionary and pulls the id data 
+    for each in studentGrades:
+        targetID = studentGrades[each]
 
-    info = [targetID]
-    grade = []
-    id_grades = c.execute("SELECT id, mark FROM courses")  
+        info = [targetID]
+        grade = []
+        id_grades = c.execute("SELECT id, mark FROM courses")  
 
-    #Then loops through the courses table to pull grades that match the student.
-    for each_courses in id_grades:
+        #Then loops through the courses table to pull grades that match the student's id.
+        for each_courses in id_grades:
         
-        courseId = each_courses[0]
-        courseGrade = each_courses[1]
+            courseId = each_courses[0]
+            courseGrade = each_courses[1]
         
-        if targetID == courseId:
-            grade.append(courseGrade)
-    print (grade)
-    info.append(grade)
+            if targetID == courseId:
+                grade.append(courseGrade)
+            
+        info.append(grade)
+        studentGrades[each] = info
 
-    studentGrades[each] = info
+look_up()
 
-#Prints the grades, Removes the grades of the student and replaces it with the average of the grades.
-for each in studentGrades:
+#Removes the grades of the student and replaces it with the average of the grades.
+# studentGrades should be studentName : [id , avgGrade]
+def avg_grade():
+    for each in studentGrades:
     
-    average = 0
-    counter = 0
-    for grade in studentGrades[each].pop():
-        average += grade
-        counter += 1
+        average = 0
+        counter = 0
+        for grade in studentGrades[each].pop():
+            average += grade
+            counter += 1
 
-    average = average / counter
-    #print (average)
-    studentGrades[each].append(average)
-    #print(studentGrades[each])
+            #calculates the avg
+            average = average / counter
+            studentGrades[each].append(average)
 
+avg_grade()
+
+#displays the student's name, id and average grade
 def display():
     command = ""
     for student in studentGrades:
@@ -67,18 +74,20 @@ def display():
 
 display()
 
+#creates a table ,peeps_avg, that inserts the values of id and avgGrade
 def create_table():
-    command = "CREATE TABLE grades (id INTEGER PRIMARY KEY, grade INTEGER)"
+    command = "CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, grade INTEGER)"
     c.execute(command)
 
     for student in studentGrades:
         avgGrade = studentGrades[student][1]
         id = studentGrades[student][0]
-        command = "INSERT INTO grades values ({0}, {1})".format(id, avgGrade)
+        command = "INSERT INTO peeps_avg values ({0}, {1})".format(id, avgGrade)
         c.execute(command)
 
 create_table()
-    
+
+#adds to the courses table, a course name, student id and mark as one row.
 def add_to_courses(courseName, studentId, mark):
     c.execute("INSERT INTO courses vales (\"{0}\", {1}, {2})".format(courseName, mark, studentId) )
     
