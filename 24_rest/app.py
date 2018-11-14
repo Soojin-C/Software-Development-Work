bf3fd6f24e3f707cca9fb78cdb7e88b55eb41948
Soojin Choi <schoi4@stuy.edu>
2018-11-14 02:59:28 -0500

    k24_rest

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,13 +1,25 @@
-from flask import Flask
+# Soojin Choi
+# SoftDev1 pd08
+# K24 -- A RESTful Journey Skyward
+# 2018-11-13
+
+from flask import Flask, render_template
 import urllib.request
+
+import json
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    urllib.request.urlopen("");
-    
-    return "hello world"
-    
+def home():
+    url = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=SXcgvyAUE0NVXPjQiVmHUd6wPidKafhrz9vCgPkU");
+    info = url.read()
+
+    dict_info = json.loads(info)
+    descrip = dict_info["title"]
+    image = dict_info["url"]
+
+    return render_template("home.html", img = image, title = descrip)
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
